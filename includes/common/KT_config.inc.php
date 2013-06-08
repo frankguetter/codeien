9f28549cd376ec1526da68a7565b8e1c22c516ff
Frank Guetter <frankguetter@gmail.com>
2013-06-08 12:58:19 +1000

    Revert "Imagic-php Check"
    This reverts commit bb81512d6a286495128dd6ccbb7f516d7dd57e23.

--- includes/common/KT_config.inc.php
+++ includes/common/KT_config.inc.php
@@ -6,7 +6,7 @@
   $KT_db_time_format = "HH:mm:ss";
   $KT_screen_date_format = "m/d/yy";
   $KT_screen_time_format = "h:mm:ss t";
-  $KT_prefered_image_lib = "imagemagick";
+  $KT_prefered_image_lib = "gd";
   $KT_prefered_imagemagick_path = "";
   $KT_prefered_tidy_path = "";
   $KT_prefered_aspell_path = "";
